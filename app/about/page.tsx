b6904e0e6b3d657c87f854cdafac700d92e7fd2f
unknown <park53046@gmail.com>
2026-07-10 10:59:43 +0900

    학교소개 여백기 조정

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,10 +3,11 @@ import Link from 'next/link';
 export default function AboutPage(){
    return (
          <div>
-            <h1 className="mt-2 text-lg leading-8 text-white-600 px-10 py-3 rounded-xl shadow-lg">
+            <p>:</p>
+            <h1 className="mt-8 text-lg leading-8 text-white-600 px-12 py-3 rounded-xl shadow-lg">
               저희 학교를 소개합니다.
             </h1>
-            <div className="mt-6 text-lg leading-8 text-white-600 px-10 py-10 rounded-xl shadow-lg"> 
+            <div className="mt-2 text-lg leading-8 text-white-600 px-10 py-10 rounded-xl shadow-lg"> 
                <p>동해삼육고등학교는 강원도 동해시에 위치한 사립 고등학교입니다.</p>
                <p>저희 학교는 1964년에 설립된 역사 깊은 학교로, 학생들에게 양질의 교육과 다양한 활동을 제공하고 있습니다.</p>
                <p>학교 연혁 페이지에서 학교의 역사와 교육 철학을 확인하실 수 있습니다.</p>
