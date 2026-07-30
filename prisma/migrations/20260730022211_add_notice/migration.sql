705277eed9b188bb3e15b09e9ae9ac3001d63900
park53046 <park53046@gmail.com>
2026-07-30 14:37:53 +0900

    홈 화면 메인 사진 이미지 변경

---
+++ prisma/migrations/20260730022211_add_notice/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Notice" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "slug" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
